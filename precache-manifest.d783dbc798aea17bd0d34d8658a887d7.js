65c8596eca286486e7e800c8c418c3eb48a034a6
st_koja <st.koja0@gmail.com>
2021-08-31 15:47:29 +0900

    Updates

---
+++ precache-manifest.d783dbc798aea17bd0d34d8658a887d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91f8c41956d3fc80ebd",
+    "url": "css/app.d40fc157.css"
+  },
+  {
+    "revision": "34ba91227beb631be77dca6f629bc3a8",
+    "url": "index.html"
+  },
+  {
+    "revision": "a91f8c41956d3fc80ebd",
+    "url": "js/app.650ac860.js"
+  },
+  {
+    "revision": "97d4fb075fe8c3687182",
+    "url": "js/chunk-vendors.4f569acb.js"
+  },
+  {
+    "revision": "4151a88353744039b13e451d0c379088",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
